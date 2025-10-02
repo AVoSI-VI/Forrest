796664d53795689c5b0305a1c33efc372049d6bc
AVoSI-VI <jdlyons@min-defcomputers.com>
2025-10-02 16:24:37 -0600

    add wish list to main cli file

---
+++ Forrest.nim
@@ -0,0 +1,52 @@
+# using cligen -> https://github.com/c-blake/cligen
+
+
+#starting with the wish list
+proc init()=
+    discard
+
+proc hash_object()=
+    discard
+
+proc cat_file()=
+    discard
+
+proc write_tree()=
+    discard
+
+proc read_tree()=
+    discard
+
+proc commit()=
+    discard
+
+proc log()=
+    discard
+
+proc checkout()=
+    discard
+
+proc tag()=
+    discard
+
+proc roll_back_file()=
+    discard
+
+proc show_oid_history()=
+    discard
+
+proc set_remote()=
+    discard
+
+proc list_remotes()=
+    discard
+
+proc clone_repo()=
+    discard
+
+proc clone_file()=
+    discard
+
+# when isMainModule:
+#   import cligen
+#   dispatchMulti([foo, help={"myRequired": "Need it!"}], [bar])
