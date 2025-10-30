406acd6e04555a03f0e391990bcb25b4501c48ce
AVoSI-VI <jdlyons@min-defcomputers.com>
2025-10-30 16:37:19 -0600

    work on write tree, piece by piece

--- cli/base.nim
+++ cli/base.nim
@@ -1,11 +1,19 @@
-import std/tables
+import std/[tables,dirs, paths, strutils, sequtils]
 
 import jsony
+
+import data
 # base wish list
 
+
 proc write_tree*(directory: string = "."): Table[string, string]=
-    
     var changes = initTable[string, string]()
+    for files in walkDirRec(Path(directory)):
+        #split out the directory for easier ignore
+        let splitUpDir = string(files).split("/")
+        if splitUpDir.contains(".git") or splitUpDir.contains(".Forrest"): #skip git directories by default
+            continue
+        let oid = data.hash_object(string(files))
     return changes
 
 proc empty_current_directory()=
